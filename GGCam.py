ef0696a05008b28447ff3c67af2b9e8b7631cd0c
balao1312 <balao1014@hotmail.com>
2021-04-05 09:09:57 +0800

    version 0404

--- GGCam.py
+++ GGCam.py
@@ -4,17 +4,20 @@ import picamera
 import time
 from pathlib import Path
 import sys
+import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(3, GPIO.IN)
 
 is_usb_mouned = call('mount -l | grep sda1', shell=True, stdout=DEVNULL, stderr=DEVNULL)
 if is_usb_mouned:
     print('\n==> usb drive not found. Trying to mount...')
-    mount_result = call(['/home/pi/mount_usb.sh'], stdout=DEVNULL, stderr=DEVNULL)
+    mount_result = call(['./mount_usb.sh'], stdout=DEVNULL, stderr=DEVNULL)
     if mount_result != 0:
         print('==> Can\'t mount usb. Exited.\n')
         sys.exit(1)
     else:
-        print('==> usb drive mounted.\n')
+        print('==> usb drive mounted.')
 
 
 usb_video_folder = Path('/mnt/usb/videos')
@@ -24,34 +27,47 @@ if not usb_video_folder.exists():
 
 temp_h264_file = Path('/home/pi/video.h264')
 
-duration = 60 
+# duration = 10000000
  
 print('\n==> Start recording ...')
-with picamera.PiCamera() as cam:
-    cam.resolution=(1920,1080)
-    cam.annotate_background = picamera.Color('black')
-    cam.start_preview()
-    cam.start_recording(str(temp_h264_file))
+
+def processing_video():
+    print('==> Processing video: ')
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    output = usb_video_folder.joinpath(f'{timestamp}.mp4')
+
+    print(f'==> Saving to {output}')
+    call(["MP4Box", "-add", f'{temp_h264_file}:fps=30', output])#, stdout=DEVNULL, stderr=DEVNULL)
+    print('\n==> Done. mp4 file saved.\n')
+    temp_h264_file.unlink()
+
+def recording():
+    with picamera.PiCamera() as cam:
+        cam.resolution=(1920,1080)
+        cam.annotate_background = picamera.Color('black')
+        cam.framerate = 30
+        cam.start_preview()
+        cam.start_recording(str(temp_h264_file))
     
-    start = datetime.now()
-    count = 0
-    while (datetime.now() - start).seconds < duration:
-        cam.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        cam.wait_recording(0.1)
-        print(count)
-        count +=1
-        time.sleep(1)
+        start = datetime.now()
+        count = 0
+        while True: #(datetime.now() - start).seconds < duration:
+            if (datetime.now() - start).seconds > 10:
+                cam.stop_preview()
+            cam.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cam.wait_recording(0.1)
+            print(count)
+            count +=1
+            if GPIO.input(3):
+                return
+            time.sleep(1)
 
-print('==> Done recording\n')
+while True:
+    if not GPIO.input(3):
+        recording()
+        processing_video()
+        print('==> Done recording\n')
 
+    time.sleep(1)
 
-timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-
-print('==> Processing video: ')
-
-output = usb_video_folder.joinpath(f'{timestamp}.mp4')
-print(f'==> Saving to {output}')
-
-call(["MP4Box", "-add", temp_h264_file, output], stdout=DEVNULL, stderr=DEVNULL)
-print('\n==> Done. mp4 file saved.\n')
 
