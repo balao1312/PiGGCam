e6f379dbbf61f814560f32536fdce14521508e05
balao1312 <balao1014@hotmail.com>
2021-04-29 17:21:44 +0800

    change fatab fs format from vfat to auto

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ config = {
     'duration': 600,
     'mount_folder': '/mnt/usb',
     'output_folder': '/mnt/usb/videos',
-    'fps': 60, 
-    #'resolution': (1920, 1080)
-    'resolution': (1280, 720)
+    'fps': 30, 
+    'resolution': (1920, 1080)
+    # 'resolution': (1280, 720)
 }
