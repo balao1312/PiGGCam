aca3d162980ec0a20e7066f0d0d55028eb3c76af
balao1312 <balao1014@hotmail.com>
2021-05-25 16:30:26 +0800

    setup script rework

--- setup.py
+++ setup.py
@@ -4,109 +4,151 @@ import pprint
 import subprocess
 import sys
 
-def clean_screen():
-    cp = subprocess.run('clear')
 
-# resulotion
-res_dic = {
-    '1': ((1920, 1080), 30),
-    '2': ((1280, 720), 60),
-    '3': ((1280, 720), 30),
-}
+class PiGGCam_Configger():
 
-print('==> Video resolution choices:\n\t1: 1920x1080,30p\n\t2: 1280x720,60p\n\t3: 1280x720,30p\n')
-while 1:
-    resolution_choice = input('\tYour choice: ')
+    def clean_screen(self):
+        cp = subprocess.run('clear')
 
-    if resolution_choice in ['1', '2', '3']:
-        break
-    else:
-        print('Please input 1, 2, or 3.')
-clean_screen()
+    def clean_deco(func):
+        def wrapper(self, *arg, **kwarg):
+            self.clean_screen()
+            result = func(self, *arg, **kwarg)
+            return result
+        return wrapper
 
-# duration
-print('==> Please input duration(secs) of each clips.\n')
-while 1:
-    try:
-        duration = int(input('\tduration(secs): '))
-        if duration < 30 or duration > 900:
-            print('value must in range 30 ~ 900 secs')
-            continue
-        break
-    except KeyboardInterrupt:
-        print()
-        sys.exit()
-    except:
-        print('value is not interger')
-        continue
-clean_screen()
+    @clean_deco
+    def set_resolution(self):
+        res_dic = {
+            '1': ((1920, 1080), 30),
+            '2': ((1280, 720), 60),
+            '3': ((1280, 720), 30),
+        }
 
-# output file location
-print('==> Please choose where to save output files:\n\t1: sd card\n\t2: usb drive.')
-print('==> Note: Choose SD card may cause frame dropping issue.\n')
-while 1:
-    output_choice = input('\toutput_location: ')
-    if output_choice in ['1', '2']:
-        break
-    else:
-        print('Please input 1 or 2.')
-        continue
+        print('Video resolution choices:\n\n\t1: 1920x1080, 30p\n\t2:  1280x720, 60p\n\t3:  1280x720, 30p\n')
+        while 1:
+            resolution_choice = input('\n\tYour choice: ')
+            if resolution_choice in ['1', '2', '3']:
+                break
+            else:
+                print('Please input 1, 2, or 3.')
+        print(res_dic[resolution_choice])
+        return res_dic[resolution_choice]
 
-output_location = 'sd card' if output_choice == '1' else 'usb drive'
-print(f'==> output videos will save to: {output_location}')
-clean_screen()
-
-# record mode
-print('==> Please choose record mode:\n\t1: non-stop recording\n\t2: motion detect recording.\n')
-while 1:
-    output_choice = input('\trecord mode: ')
-    if output_choice in ['1', '2']:
-        break
-    else:
-        print('Please input 1 or 2.')
-        continue
-
-record_mode = 'non-stop' if output_choice == '1' else 'motion'
-print(f'==> record mode is set to: {record_mode}')
-clean_screen()
-
-# if record mode == motion
-if record_mode == 'motion':
-    print('==> Please input motion detection interverl(secs):\n')
-    while 1:
-        try:
-            motion_interval = int(input('\tintervals(secs): '))
-            if motion_interval < 60 or motion_interval > 300:
-                print('value must be > 60 secs and < 300 secs')
+    @clean_deco
+    def set_duration(self):
+        print('Please input max duration(secs) of each clips.\n\t* range: 30 secs ~ 900 secs\n')
+        while 1:
+            try:
+                duration = int(input('\n\tduration(secs): '))
+                if duration < 30 or duration > 900:
+                    print('\nvalue must in range 30 ~ 900 secs')
+                    continue
+                break
+            except KeyboardInterrupt:
+                print()
+                sys.exit()
+            except:
+                print('value is not interger')
                 continue
-            break
-        except KeyboardInterrupt:
-            print()
-            sys.exit()
-        except:
-            print('value is not interger')
-            continue
+        return duration
 
-    print(f'==> motion_interval: {motion_interval} secs')
-else:
-    motion_interval = 0
-clean_screen()
+    @clean_deco
+    def set_output_location(self):
+        print('Please choose where to save output files:\n\n\t1: sd card\t*may cause frame dropping issue.\n\t2: usb drive.\n')
+        while 1:
+            output_choice = input('\n\toutput_location: ')
+            if output_choice in ['1', '2']:
+                break
+            else:
+                print('Please input 1 or 2.')
+                continue
+        return 'sd card' if output_choice == '1' else 'usb drive'
+
+    @clean_deco
+    def set_record_mode(self):
+        print('Please choose record mode:\n\n\t1: non-stop recording\n\t2: motion detect recording.\n')
+        while 1:
+            output_choice = input('\n\trecord mode: ')
+            if output_choice in ['1', '2']:
+                break
+            else:
+                print('Please input 1 or 2.')
+                continue
+
+        self.record_mode = 'non-stop' if output_choice == '1' else 'motion'
+        return self.record_mode
+
+    @clean_deco
+    def set_motion_interval(self):
+        if self.record_mode == 'motion':
+            print('Please input motion detection interverl(secs):\n')
+            print('* Description: \n\tStop recording signal will be sent if no movement detected within this time interval past.\n\trange: 60 secs ~ 300 secs\n')
+            while 1:
+                try:
+                    motion_interval = int(input('\n\tintervals(secs): '))
+                    if motion_interval < 60 or motion_interval > 300:
+                        print('\nvalue must be in range 60 secs ~ 300 secs')
+                        continue
+                    break
+                except KeyboardInterrupt:
+                    print()
+                    sys.exit()
+                except:
+                    print('value is not interger')
+                    continue
+            return motion_interval
+        else:
+            return 0
+
+    @clean_deco
+    def do_config(self):
+        resolution = self.set_resolution()
+        self.config = {
+            'resolution': resolution[0],
+            'fps': resolution[1],
+            'duration': self.set_duration(),
+            'output_location': self.set_output_location(),
+            'record_mode': self.set_record_mode(),
+            'motion_interval': self.set_motion_interval(),
+        }
+
+    @clean_deco
+    def show_config(self):
+        print(f'''Your configures are list below:
+
+        Resolution:             {self.config['resolution'][0]} x {self.config['resolution'][1]}
+        Frames Per Second:      {self.config['fps']} 
+        Duration:               {self.config['duration']} secs
+        Output Location:        {self.config['output_location']}
+        Record Mode:            {self.config['record_mode']}
+        Motion Detect Interval: {self.config['motion_interval']} secs        # set to 0 if record mode = non-stop
+
+        ''')
+
+    @clean_deco
+    def run(self):
+        while 1:
+            self.do_config()
+            self.show_config()
+
+            is_confirmed = input('\tConfirm and Save? (y/n):')
+            if is_confirmed == 'y':
+                break
+            else:
+                do_reset = input('\n\tRedo config? (y/n):')
+                if do_reset == 'y':
+                    continue
+                else:
+                    print('\n\nLeaving without any change.')
+                    return
+
+        with open('config.py', 'w') as f:
+            f.write(f'config = {pprint.pformat(self.config)}')
+
+        print('\n\n==> Config updated.')
 
 
-# -------------------------------------------------------------------------------------------------------------------
-print(f'==> resolution selected: {res_dic[resolution_choice]}')
-print(f'==> duration: {duration} secs')
-
-config = {
-    'fps': res_dic[resolution_choice][1],
-    'resolution': res_dic[resolution_choice][0],
-    'duration': duration,
-    'output_location': output_location,
-    'record_mode': record_mode,
-    'motion_interval': motion_interval,
-}
-
-with open('config.py', 'w') as f:
-    f.write(f'config = {pprint.pformat(config)}')
-
-print('Setting updated.')
+if __name__ == '__main__':
+    configger = PiGGCam_Configger()
+    configger.run()
