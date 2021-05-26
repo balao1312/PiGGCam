6b1e9b6789977c2c51e7a2b6b5d2c5eb98638c4e
balao1312 <balao1014@hotmail.com>
2021-05-27 06:48:08 +0800

    add some test example

--- test_gpio_example/led_two_color.py
+++ test_gpio_example/led_two_color.py
@@ -3,22 +3,14 @@ from gpiozero import Button, LED
 
 led_green = LED(26)
 led_red = LED(19)
-button = Button(23)
 
 while 1:
-    if button.is_pressed:
-        print('pressed')
-        target = led_green
-        target.on()
-        time.sleep(0.1)
-        target.off()
-        time.sleep(0.1)
+    led_green.on()
+    time.sleep(0.5)
+    led_green.off()
+    time.sleep(0.5)
 
-    else:
-        print('not pressed')
-        target = led_red
-        target.on()
-        time.sleep(0.1)
-        target.off()
-        time.sleep(0.1)
-
+    led_red.on()
+    time.sleep(0.5)
+    led_red.off()
+    time.sleep(0.5)
